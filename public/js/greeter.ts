f94ed624731d74b305e4b8b49ac48af8a77dc80c
alex-gru <grusch.alex@gmail.com>
2017-04-29 18:17:50 +0200

    fetch and display courses; renaming

--- public/js/greeter.ts
+++ public/js/greeter.ts
@@ -1,10 +1,14 @@
 declare let greeting: any;
 
-const app = angular.module("greetingApp", []);
+const app = angular.module("app", []);
 
-app.controller("greetingCtrl", ($scope) => {
+app.controller("ctrl", ($scope, $http) => {
 
     $scope.greeting = greeting;
     $scope.name = "Jane Doe";
     $scope.description = "This is a dockerized MEAN webapp, implemented in Typescript, with full support for hot code reload and debugging with an IDE.";
+
+    $http.get("/courses").then((response) => {
+        $scope.courses = response.data;
+    });
 });
