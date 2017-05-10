1dac6e144250e74929af0541ce1e1f1fec18ca29
Alexander Gruschina <grusch.alex@gmail.com>
2017-05-10 08:46:56 +0200

    handle module.compulsory via radio buttons in create-module; fix form color in course-create

--- app/components/modules/module-create.component.ts
+++ app/components/modules/module-create.component.ts
@@ -1,7 +1,7 @@
 app.component('moduleCreate', {
   templateUrl: '/components/modules/module-create.component.html',
   controller: ($scope, $location, apiService) => {
-
+    $scope.compulsory = "true";
     $scope.createModule = () => {
       apiService.createModule($scope.name, $scope.compulsory, $scope.description, $scope.objective)
         .then(() => {
