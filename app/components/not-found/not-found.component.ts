2dc4363ebf78d9a81f055a7ffa639e4ef84da08a
alex-gru <grusch.alex@gmail.com>
2017-05-18 20:58:13 +0200

    provide back button in "not-found" to navigate back to last correct site (-2 steps, -1 was the URL which led to not-found)

--- app/components/not-found/not-found.component.ts
+++ app/components/not-found/not-found.component.ts
@@ -1,5 +1,8 @@
 app.component('notFound', {
   templateUrl: '/components/not-found/not-found.component.html',
-  controller: () => {
+  controller: ($scope, $window) =>
+
+    $scope.back = () => {
+      $window.history.go(-2); // -1 = led to not-found, -2 = last correct site
   }
 });
